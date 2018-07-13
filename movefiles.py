742c2fa256ebfb798faf4b363e6712a53c05f057
shyam lohar <shyamlohar19996@gmail.com>
2018-07-13 19:08:26 +0530

    added few new features.

--- movefiles.py
+++ movefiles.py
@@ -1,66 +1,116 @@
 import os
 import glob
 import shutil
+import sys
+import argparse
 
-if os.getcwd().split('\\')[-1] == "Desktop":
-    current_directory = os.getcwd()
-    move_directory = os.path.join(os.getcwd(),'Arrangedfiles')
+parser = argparse.ArgumentParser(description = 'make your directory look clean again')
+parser.add_argument('-e','--extension', metavar = '' ,help = 'extension of file you wanna move to selected folder')
+parser.add_argument('-f','--folder', metavar = '' ,help = 'name of folder you wanna move your files to')
+args = parser.parse_args()
 
-    if not "Arrangedfiles" in os.listdir():
+current_directory = os.getcwd()
+move_directory = os.path.join(os.getcwd(),'Arrangedfiles')
+
+def creat_folder():
+    if not 'Arrangedfiles' in os.listdir():
+        os.system('mkdir Arrangedfiles')
+        print('created Arrangedfiles folder and your files will be move to that directory');
+
+def move_all_smart():
+    # current_directory = os.getcwd()
+    # move_directory = os.path.join(os.getcwd(),'Arrangedfiles')
+
+    if not 'Arrangedfiles' in os.listdir():
         print('nahi hai folder bana bhai');
         os.system('mkdir Arrangedfiles')
 
     files = os.listdir()
 
     text_files = glob.glob('*.txt')
-    image_files = glob.glob('*.png') + glob.glob('*.jpeg') + glob.glob('*.gif') + glob.glob('*.bmp') + glob.glob('*.jpg') + glob.glob('*.avi')
 
-    video_files = glob.glob('*.mp4') + glob.glob('*.avi') + glob.glob('*.mp4') + glob.glob('*.flv') + glob.glob('*.mkc') + glob.glob('*.wmv') +               glob.glob('*.mov') + glob.glob('*.3gp') + glob.glob('*.mpg')
+    image_files = glob.glob('*.png') + glob.glob('*.jpeg') + glob.glob('*.gif') + glob.glob('*.bmp') + glob.glob('*.jpg') 
 
-    compressed_files = glob.glob('*.zip') + glob.glob('*.rar') + glob.glob('*.7z') + glob.glob("*.tar")
+    video_files = glob.glob('*.mp4') + glob.glob('*.flv') + glob.glob('*.mkv') + glob.glob('*.wmv') + glob.glob('*.avi') + glob.glob('*.mov') + glob.glob('*.3gp') + glob.glob('*.mpg')
+
+    compressed_files = glob.glob('*.zip') + glob.glob('*.rar') + glob.glob('*.7z') + glob.glob('*.tar')
 
     document_files = glob.glob('*.pdf') + glob.glob('*.docx') + glob.glob('*.doc') + glob.glob('*.xls') + glob.glob                                             ('*.xlsm') + glob.glob('*.ppt') + glob.glob('*.pptx')
 
 
     for file in text_files:
         src = os.path.join(current_directory,file)
-        if not 'textfiles' in move_directory:
+        if not 'textfiles' in os.listdir(move_directory):
             os.system(f'cd {move_directory} && mkdir textfiles')
         dst = os.path.join(move_directory,'textfiles')
         shutil.move(src,dst)
-        print('all text files moved')
-    
+    print('all text files moved')
+
     for file in image_files:
         src = os.path.join(current_directory,file)
-        if not 'images' in move_directory:
+        if not 'images' in os.listdir(move_directory):
             os.system(f'cd {move_directory} && mkdir images')
         dst = os.path.join(move_directory,'images')
+        print(src,'src')
         shutil.move(src,dst)
-        print('all images moved')
+    print('all images moved')
 
     for file in video_files:
+        print(file)
         src = os.path.join(current_directory,file)
-        if not 'images' in move_directory:
+        if not 'videos' in os.listdir(move_directory):
             os.system(f'cd {move_directory} && mkdir videos')
         dst = os.path.join(move_directory,'videos')
         shutil.move(src,dst)
-        print('all videos moved')
+    print('all videos moved')
 
     for file in compressed_files:
         src = os.path.join(current_directory,file)
-        if not 'images' in move_directory:
+        if not 'compressed_files' in os.listdir(move_directory):
             os.system(f'cd {move_directory} && mkdir compressed_files')
         dst = os.path.join(move_directory,'compressed_files')
         shutil.move(src,dst)
-        print('all videos moved')
+    print('all videos moved')
 
     for file in document_files:
         src = os.path.join(current_directory,file)
-        if not 'documents' in move_directory:
+        if not 'documents' in os.listdir(move_directory):
             os.system(f'cd {move_directory} && mkdir documents')
         dst = os.path.join(move_directory,'documents')
         shutil.move(src,dst)
-        print('all documents moved')
+    print('all documents moved')
 
     print('{:*^80}'.format('all done'))
 
+def move_manually(extension,folder):
+    files_to_move = glob.glob(f'*.{extension}')
+    creat_folder()
+    for file in files_to_move:
+        src = os.path.join(current_directory,file)
+        if not folder in os.listdir(move_directory):
+            os.system(f'cd {move_directory} && mkdir {folder}')
+        dst = os.path.join(move_directory,folder)
+        shutil.move(src,dst)
+    print('moved all your files in your desired folder')
+
+if __name__ == '__main__':
+
+    if not args.extension and args.folder:
+        print('specify extension of file which you wanna move to specifield folder and rerun the script')
+
+    elif args.extension and not args.folder:
+        print('specify folder in which you wanna move all you files and re run the script again');
+
+    elif args.extension and args.folder:
+        print(f'moving all files with {args.extension} extension to {args.folder} folder')
+        move_manually(args.extension,args.folder)
+
+    else:
+        k = input('reply with "y" if you wanna arrange all your files or reply with "n" if you wanna exit \n')
+        if k.lower() == 'y':
+            print ("yes moving everything smartly")
+            move_all_smart()
+        elif k.lower() == 'n':
+            print ("you are free enough to do it on your own it seems if not rerun script and use appropriate options use --help command while running script for help")
+
+    
---
+++ readme.md
@@ -0,0 +1,53 @@
+# File Arranger
+
+#### Small Script to keep your folders clean from pile of files.
+##### just run python script in folder you want to organize and script will do the job
+
+### Example
+---
+##### you have C:\Users\shyam directory with following files 
+
+* hello.png
+* bye.mp4
+* dd.avi
+* python.mkv
+* logo.bmp
+* resume.pdf
+* data.zip
+* compressed_images.7z
+
+##### And you want to organize this folder.
+
+##### Open Terminal in that folder and run python script.
+###### if you dont know how to use this script just put this script in folder you wanna organize and  runs script in terminal using command ```python movefiles.py``` just make sure you have installed python 3 on your machine.
+
+##### Script will arrange file for you like shown below
+
+```
+   Arrangedfiles/
+   		textfiles/..*txt files here*
+        images/..*image files here*
+        videos/..*video files here*
+        compressed_files/..*compressed files here*
+        documents/.. *document files here* 
+   ```
+   
+ ##### If you wanna move files with certain extension to desired folder you can pass arguments while running script as shown below 
+ 
+ ``` python movefiles.py -e <file extension here> -f <folder name here>```
+#### 					or
+ ``` python movefiles.py -extension <file extension here> -f <folder name here>```
+ 
+ ### Example
+ ---
+ ```python movefiles.py -e py -f pythonfiles```
+ 
+ ###### this will move all your files with .py extensions in 
+ ```
+ Arrangedfiles/
+      pythonfiles..
+   ```
+  
+  
+######  For help Related Script run
+```python movefiles.py -help```
